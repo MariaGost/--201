a70b407284c7c45bca65f8aac657b4ca5d544831
MATH RUDN <rudn.math@outlook.com>
2020-03-05 10:32:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace нмт_201
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
